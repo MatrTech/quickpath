0eef24dd617bf7d4ceb32aeb054e9218c52a9968
Mark Visschers <visschers.m@gmail.com>
2024-10-12 13:02:24 +0200

    updates workflow

--- quickpath.psm1
+++ quickpath.psm1
@@ -1,4 +1,3 @@
-. "$PSScriptRoot/private/Alias-Helper.ps1"
 $Public = @( Get-ChildItem -Path $PSScriptRoot\public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 $Classes = @( Get-ChildItem -Path $PSScriptRoot\classes\*.ps1 -Recurse -ErrorAction SilentlyContinue )
