602185f9cf81be6c383a0823752013530b2bc983
Mark Visschers <visschers.m@gmail.com>
2024-10-12 14:49:09 +0200

    re-adds tests

--- tests/quickpath.Tests.ps1
+++ tests/quickpath.Tests.ps1
@@ -1,6 +1,6 @@
-Import-Module "$PSScriptRoot/../quickpath.psd1" -Force
+Import-Module "$PSScriptRoot\..\quickpath.psd1" -Force
 
-Write-Host "ROOT FOLDER: $PSScriptRoot"
+
 InModuleScope quickpath {
     Describe 'quickpath' {
         BeforeEach {
@@ -8,9 +8,19 @@ InModuleScope quickpath {
             Mock Set-Location
             Mock Import-Aliases -Verifiable
         }
-        It 'Failing test' {
-            $result = $true
-            $result | Should -Be  $true
+        context 'init' {  
+            It "Import-Aliases Gets called" {
+                { qp "some-valid-path" } | Should -Not -Throw
+                
+                Assert-MockCalled Import-Aliases -Exactly 1 -Scope It
+            }
+        }
+        context 'NavigateByPath' {
+            It "Valid alias, should not throw" {
+                { qp "some-valid-alias" } | Should -Not -Throw
+
+                Assert-MockCalled Set-Location -Exactly 1 -Scope It
+            }
         }
     }
 }
