76f8823e3628851d54720f9224cbf7ba537f72e3
Mark Visschers <visschers.m@gmail.com>
2024-10-12 12:54:57 +0200

    updates workflow

---
+++ tests/quickpath.Tests.ps1
@@ -0,0 +1,16 @@
+Import-Module "quickpath.psd1" -Force
+
+Write-Host "$PSScriptRoot"
+InModuleScope quickpath {
+    Describe 'quickpath' {
+        BeforeEach {
+            Mock Test-Path { return $true }
+            Mock Set-Location
+            Mock Import-Aliases -Verifiable
+        }
+        It 'Failing test' {
+            $result = $true
+            $result | Should -Be  $true
+        }
+    }
+}
