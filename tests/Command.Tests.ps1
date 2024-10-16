a08bf63cd9dce9be69c5467d97889ebcc4dedfe8
Mark Visschers <visschers.m@gmail.com>
2024-10-16 13:58:42 +0200

    adds invokefunction test

--- tests/Command.Tests.ps1
+++ tests/Command.Tests.ps1
@@ -30,4 +30,22 @@ Describe 'Command tests' {
             Assert-MockCalled Write-host -Exactly 1 -Scope It
         }
     }
+    Context 'InvokeFunction' {
+        It 'Define command with function, function can gets called' {
+            Mock Write-Host { "Hello, TestCommand" } -Verifiable
+
+            $command = [Command]::new("TestCommand", "Write-Host")
+            $command.InvokeFunction()
+
+            Assert-MockCalled Write-host -Exactly 1 -Scope It
+        }
+        It 'Function with help argument, PrintHelp gets called' {
+            Mock Write-Host -Verifiable
+
+            $command = [Command]::new("TestCommand", "Write-Host")
+            $command.InvokeFunction("help") | Should 
+
+            Assert-MockCalled Write-Host -Exactly 1 -Scope It
+        }
+    }
 }
