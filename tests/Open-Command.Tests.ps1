7a7eb87afdbb42308a3fec49e9e5bae72e3ca8b2
Mark Visschers <visschers.m@gmail.com>
2025-01-17 23:23:59 +0100

    adds more tests for Get-Command

--- tests/Open-Command.Tests.ps1
+++ tests/Open-Command.Tests.ps1
@@ -6,4 +6,28 @@ Describe 'Open Command tests' {
         Mock Get-Alias { return $null }
         { Open-Command } | Should -Throw
     }
+
+    It 'Program not found throws exception' {
+        Mock Get-Alias { return "hello world" }
+        Mock Get-Command { return $null }
+        { Open-Command } | Should -Throw
+    }
+
+    It 'Path could not be found throws exception' {
+        Mock Get-Alias { return "hello world" }
+        Mock Get-Command { }
+        Mock Test-Path { return $null }
+        { Open-Command } | Should -Throw 
+    }
+
+    It 'Valid command Start-Process called' {
+        Mock Get-Alias { return "hello world" }
+        Mock -CommandName Get-Command -MockWith { @{} }
+        Mock -CommandName Test-Path -MockWith { $true }
+        Mock Start-Process
+
+        Open-Command "some-program" "some-alias" $true
+
+        Assert-MockCalled Start-Process -Exactly 1 -Scope It 
+    }
 }
